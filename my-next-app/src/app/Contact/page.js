b6a920f7d2b9e571e772167263fc0dff115e6e7a
aditya2265 <adityajs5869@gmail.com>
2025-07-31 23:02:04 +0530

    first commit

---
+++ my-next-app/src/app/Contact/page.js
@@ -0,0 +1,13 @@
+const Contact = () => (
+  <div>
+    <h1>Contact Us</h1>
+    <p>For any quary you can contact our support team with the following mail or contact number.</p>
+   <br/>
+    <p> Email : aditya140@gmail.com</p>
+    <br/>
+    <p>contact No : +91 9856214720</p>
+
+  </div>
+  
+);
+export default Contact;
---
+++ my-next-app/src/app/page.js
@@ -0,0 +1,13 @@
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <p>Welcome to Our Responsive Next.js Application
+Step into a seamless web experience with our modern,
+ responsive homepage built using Next.js. Designed for performance and user engagement,
+  this site features dynamic routing, intuitive navigation, and a sleek dark/light theme toggle for optimal user comfort. 
+  Whether you're exploring our services or getting to know our team, 
+  this homepage offers a polished introduction to a robust and scalable web solution.</p>
+  </div>
+);
+export default Home;
